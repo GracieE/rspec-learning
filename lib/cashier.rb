079b52d37d0c6aa924728e9a87789644921f74d1
Gracie Elqura <gracie.elqura@gmail.com>
2013-09-10 18:40:26 -0400

    wrote first test, pong

---
+++ lib/cashier.rb
@@ -0,0 +1,6 @@
+class Cashier
+
+  def self.make_change(cents)
+    [1, 1, 1, 1]
+  end
+end
---
+++ spec/cashier_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/cashier.rb"
+
+describe Cashier do
+  it "is a defined class" do
+    lambda {
+      Cashier
+    }.should_not raise_error
+  end
+
+  describe "when given 4 cents" do
+    it "returns 4 pennies" do
+      Cashier.make_change(4).should == [1, 1, 1, 1]
+    end
+  end
+end
