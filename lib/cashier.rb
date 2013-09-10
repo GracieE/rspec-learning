1cafea9723a4e0d7a7861789c6b4fe63103837aa
Chad W Pry <chad.pry@gmail.com>
2013-09-10 18:45:24 -0400

    add other penny case

--- lib/cashier.rb
+++ lib/cashier.rb
@@ -1,6 +1,12 @@
 class Cashier
 
   def self.make_change(cents)
-    [1, 1, 1, 1]
+    array = []
+
+    (1..cents).to_a.each do
+      array << 1
+    end
+
+    array
   end
 end
--- spec/cashier_spec.rb
+++ spec/cashier_spec.rb
@@ -7,6 +7,12 @@ describe Cashier do
     }.should_not raise_error
   end
 
+  describe "when given 2 cents" do
+    it "returns 2 pennies" do
+      Cashier.make_change(2).should == [1, 1]
+    end
+  end
+
   describe "when given 4 cents" do
     it "returns 4 pennies" do
       Cashier.make_change(4).should == [1, 1, 1, 1]
